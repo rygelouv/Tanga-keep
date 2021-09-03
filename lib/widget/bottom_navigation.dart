c169a02b2297c613c68c12223f5e8023e0931c8e
sadatetchamouza <sadatetchamouza@gmail.com>
2021-09-03 19:48:35 +0000

    Create widget for bottom navigation
    - Created the widget file
    - Update Home to use the widget
    - Use the same widget on Not screen

---
+++ lib/widget/bottom_navigation.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatelessWidget {
+  const BottomNavigation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      color: Colors.blueGrey,
+      notchMargin: 2.0,
+      clipBehavior: Clip.antiAlias,
+      child: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.house_rounded), label: "Home"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.watch_later), label: "Recent"),
+      ],
+          selectedItemColor: Theme.of(context).accentColor
+      ),
+    );;
+  }
+}
